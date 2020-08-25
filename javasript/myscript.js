29a997c02f6e1e1c240d84d8ee4f3077eedaba7f
Iscar Joseph <iscarjo@gmail.com>
2020-08-24 20:52:01 -0400

    fixed some layout issues and added button still have to clean up my form mark up

---
+++ javasript/myscript.js
@@ -0,0 +1,6 @@
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5");
+xhr.done(function(data) { console.log("success got data", data); });
+
+const api = "YfQo69W7isZ6aYVWbEmOdEpqjnho48fR";
+
+const search = "http://api.giphy.com/v1/gifs/search"
